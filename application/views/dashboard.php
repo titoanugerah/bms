1d91e3a5afd33a07f4c1f1b80b74d2527af7f3bc
Tito Anugerah Maharizky <titoanugerah@student.undip.ac.id>
2020-09-13 00:28:49 +0700

    modify dashboard templactes:

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,4 +1,3 @@
 <center>
-<h2>Selamat datang di IT Asset Information System</h2>
+<h2>Selamat datang di Backup Monitoring System</h2>
 </center>
-<img src="<?php echo base_url('./assets/picture/dashboard.jpeg')?>" style="max-height:700px;">
