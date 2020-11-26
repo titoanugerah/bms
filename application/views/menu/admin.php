79b4ca8d7be15aa94cacd2e68afde4b877f8bae9
Tito Anugerah Maharizky <titoanugerah@student.undip.ac.id>
2020-11-26 18:22:58 +0700

    added spv menu on admin

--- application/views/menu/admin.php
+++ application/views/menu/admin.php
@@ -54,3 +54,17 @@
     <p>Data Set</p>
   </a>
 </li>
+
+<li class="nav-item active" >
+  <a href="<?php echo base_url('backup'); ?>">
+    <i class="fas fa-wrench"></i>
+    <p>Backup Aplikasi/Job</p>
+  </a>
+</li>
+
+<li class="nav-item active" >
+  <a href="<?php echo base_url('issue'); ?>">
+    <i class="fas fa-sticky-note"></i>
+    <p>Laporan Masalah</p>
+  </a>
+</li> 
